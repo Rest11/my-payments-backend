afc18df7efb0ac6e26104624a8e1c79f6e9aac9e
maxim_shinkarenko-cr <maxim.shinkarenko.cr@gmail.com>
2019-02-14 14:34:19 +0200

    changed Readme file

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -63,16 +63,14 @@ export class PaymentService {
                     dateFormatField,
                 ],
                 [
-                    // sequelize.fn('count', sequelize.col(DatabaseContract.Donations.COLUMN_FK_USER_EXTERNAL_ID)),
-                    sequelize.fn('sum', sequelize.col(DatabaseContract.Donations.COLUMN_FK_USER_EXTERNAL_ID)),
+                    sequelize.fn('count', sequelize.col(DatabaseContract.Donations.COLUMN_FK_USER_EXTERNAL_ID)),
                     usersCountField,
                 ],
-                // DatabaseContract.Donations.COLUMN_FK_USER_EXTERNAL_ID
+                // DatabaseContract.Donations.COLUMN_FK_USER_EXTERNAL_ID,
             ],
             group: [
                 dateFormatField,
                 // DatabaseContract.Donations.COLUMN_FK_USER_EXTERNAL_ID,
-                // DatabaseContract.Donations.PROPERTY_STATUS,
             ],
         };
 
@@ -94,7 +92,6 @@ export class PaymentService {
             ],
             group: [
                 this.dateFormatField,
-                DatabaseContract.Donations.PROPERTY_STATUS,
             ],
         };
 
@@ -116,7 +113,6 @@ export class PaymentService {
             ],
             group: [
                 this.dateFormatField,
-                DatabaseContract.Donations.PROPERTY_STATUS,
             ],
         };
 
